a59e89ecd883badfadd4f03648b35cfb74a74ae1
Wally Thornton <wfthornton@gmail.com>
2015-12-20 15:52:12 -0600

    Fixed whitespace regex, added DFM creators

--- modelBuilder.R
+++ modelBuilder.R
@@ -26,40 +26,54 @@ UStweets <- readLines("corpus/final/en_US/en_US.twitter.txt", skipNul = TRUE)
 baseClean <- function(text) {
     new <- gsub(" @[a-zA-z]+| #[a-zA-z]+", "", text)
     new <- gsub("[[:digit:]]+", "", new)
+    # Employing negative lookahead so apostrophes not subbed; Perl needed to make it work right
     new <- gsub("(?!')[[:punct:]]+", "", new, perl = T)
+    new <- gsub("\\s\\s", " ", new)
     return(new)
 }
 UStweets <- baseClean(UStweets)
 USblogs <- baseClean(USblogs)
 USnews <- baseClean(USnews)
 
+# To detect double words, with Perl=T: "\\b(\\S+?)\\1\\S*\\b" and then insert with \1
 
+## TODO: MAKE PROFANITY FILTER WORK
+# Remove profanity
+# From Shutterstock list at https://github.com/shutterstock/List-of-Dirty-Naughty-Obscene-and-Otherwise-Bad-Words
+curseDict <- read.csv("profanities.csv", stringsAsFactor=F, quote = "")
+curseDict <- as.vector(curseDict[,1])
+curseDict <- paste0(' ', curseDict, ' ')
 
-######### Combine into corpus
+profanityClean <- function(text) {
+    for(i in length(curseDict)) {
+        new <- gsub(curseDict[i], "", text)
+    }
+    return(new)
+}
+UStweets <- profanityClean(UStweets)
+USblogs <- profanityClean(USblogs)
+USnews <- profanityClean(USnews)
+
+######### Combine into corpus and generate ngrams
 # Take random 10,000-line samples from each source and then combine into a corpus
 set.seed(42); n <- USnews[sample(length(USnews), 10000)]
 set.seed(42); b <- USblogs[sample(length(USblogs), 10000)]
 set.seed(42); t <- UStweets[sample(length(UStweets), 10000)]
 corpus <- corpus(c(n,b,t))
 
-#tolower, strip whitespace and de-profane
-# From Shutterstock list at https://github.com/shutterstock/List-of-Dirty-Naughty-Obscene-and-Otherwise-Bad-Words
-curseDict <- as.character(read.csv("profanities.csv", stringsAsFactor=F))
-corpus <- removeFeatures(corpus, curseDict)
+# Keywords in context, 3 words of context
+kwic(corpus, "love", 3)
 
 dfm1 <- dfm(corpus, toLower = T, removePunct = F, removeTwitter = T, stem = F)
-dfm2 <- dfm(corpus, concatenator = " ", verbose = F, ngrams = 2)
-dfm3 <- dfm(corpus, concatenator = " ", verbose = F, ngrams = 3)
-topfeatures(dfm, 200)  # 20 top words
+dfm2 <- dfm(corpus, toLower = T, removePunct = F, removeTwitter = T, stem = F, concatenator = " ", ngrams = 2)
+dfm3 <- dfm(corpus, toLower = T, removePunct = F, removeTwitter = T, stem = F, concatenator = " ", ngrams = 3)
+dfm4 <- dfm(corpus, toLower = T, removePunct = F, removeTwitter = T, stem = F, concatenator = " ", ngrams = 4)
+topfeatures(dfm3, 20)  # 20 top words
 
-#or\
+# Another way to look at it is collocations (bigrams, trigrams)
+colos <- collocations(corpus, method="all")
 
 
-corpus <- Corpus(VectorSource(c(n,b,t)))
+############## Build models
 
 
-corpus <- tm_map(corpus, content_transformer(tolower))
-corpus <- tm_map(corpus, removeNumbers)
-corpus <- tm_map(corpus, stripWhitespace)
-corpus <- tm_map(corpus, removeWords, as.matrix(curseDict))
-dtm <- DocumentTermMatrix(corpus, control = list(minWordLength = 2))
